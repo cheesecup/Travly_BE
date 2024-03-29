3275b18acc7057034751b9e5425fc2898113307c
Seungmi97 <mi.seung97@gmail.com>
2024-03-30 03:05:45 +0900

    FEAT: 카카오 사용자 정보로 회원가입

--- src/main/java/com/travelland/domain/Member.java
+++ src/main/java/com/travelland/domain/Member.java
@@ -1,16 +1,15 @@
 package com.travelland.domain;
 
 import com.travelland.constant.Role;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Member {
 
     @Id
@@ -19,15 +18,29 @@ public class Member {
 
     private Long socialId;
 
+    @Column(length = 30)
     private String email;
 
     private String password;
 
+    @Column(length = 15)
     private String nickname;
 
+    @Column(length = 8)
     private String gender;
 
-    private LocalDateTime birth;
+    private LocalDate birth;
 
+    @Column(length = 10)
     private Role role;
+
+    public Member(Long socialId, String email, String password, String nickname, String gender, LocalDate birth, Role role) {
+        this.socialId = socialId;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.birth = birth;
+        this.role = role;
+    }
 }
--- src/main/java/com/travelland/dto/KakaoUserInfoDto.java
+++ src/main/java/com/travelland/dto/KakaoUserInfoDto.java
@@ -2,7 +2,6 @@ package com.travelland.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
@@ -11,6 +10,6 @@ public class KakaoUserInfoDto {
     private String nickname;
     private String email;
     private String name;
-    private String birthday;
+    private String birth;
     private String gender;
 }
