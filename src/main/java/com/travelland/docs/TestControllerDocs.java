ac1894b59645029b8d8194bc0fb187cb223311ab
cheesecup <kdy8252@naver.com>
2024-03-29 15:03:45 +0900

    Swagger 설정

---
+++ src/main/java/com/travelland/docs/TestControllerDocs.java
@@ -0,0 +1,22 @@
+package com.travelland.docs;
+
+import com.travelland.dto.MemberRequestDto.LoginRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Tag(name = "Swagger Example", description = "스웨거 작성 예시 API")
+public interface TestControllerDocs {
+
+    @Operation(summary = "스웨거 테스트", description = "스웨거 테스트 예시 API")
+    ResponseEntity<String> swaggerTest(@RequestBody LoginRequestDto requestDto);
+
+    @Operation(summary = "test method", description = "test method API")
+    ResponseEntity<String> test();
+
+    @Operation(summary = "SwaggerTest", description = "swagger Test API")
+    ResponseEntity postRequest(@RequestParam String name);
+
+}
