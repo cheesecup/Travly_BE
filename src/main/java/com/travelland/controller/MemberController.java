da37ce93a663e29fefdd3a9c7a2689ad43d84205
Seungmi97 <mi.seung97@gmail.com>
2024-03-30 20:07:04 +0900

    FEAT: 카카오 디벨롭 앱 프론트와 통일 및 코드 컨벤션 통일

--- src/main/java/com/travelland/controller/MemberController.java
+++ src/main/java/com/travelland/controller/MemberController.java
@@ -1,32 +1,41 @@
 package com.travelland.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.travelland.global.jwt.JwtUtil;
 import com.travelland.service.KakaoService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class MemberController {
 
     private final KakaoService kakaoService;
 
     @GetMapping("/login/kakao")
-    public ResponseEntity<?> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+    public ResponseEntity<Map<String, Boolean>> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
         String token = kakaoService.kakaoLogin(code);
 
+        // Barer 조심!!!!!
         Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, token.substring(7));
         cookie.setPath("/");
         response.addCookie(cookie);
 
-        return ResponseEntity.ok().build();
+        Map<String, Boolean> result = new HashMap<>();
+        result.put("isSuccess", true);
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
