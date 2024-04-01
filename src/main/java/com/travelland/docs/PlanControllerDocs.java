be135e701525ebedb2197baea76c6de2622cdbad
cheesecup <kdy8252@naver.com>
2024-04-01 14:12:59 +0900

    STYLE: 응답 DTO 클래스 표시

--- src/main/java/com/travelland/docs/PlanControllerDocs.java
+++ src/main/java/com/travelland/docs/PlanControllerDocs.java
@@ -5,7 +5,6 @@ import com.travelland.dto.PlanDto;
 import com.travelland.dto.UnitPlanDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,20 +14,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface PlanControllerDocs {
 
     @Operation(summary = "Plan 작성", description = " ")
-    ResponseEntity<PlanDto.CreateResponse> createPlan(@RequestBody PlanDto.CreateRequest request);
+    ResponseEntity createPlan(@RequestBody PlanDto.CreateRequest request);
 
     @Operation(summary = "Plan 전체조회", description = "전체조회, /plans?page=0&size=20&sortBy=createdAt&isAsc=false, page는 1부터")
-    ResponseEntity<Page<PlanDto.ReadResponse>> readPlanList(
+    ResponseEntity readPlanList(
             @RequestParam int page, @RequestParam int size, @RequestParam String sortBy, @RequestParam boolean isAsc);
 
     @Operation(summary = "Plan 상세조회", description = "상세조회, planId로 조회, userId는 로그인구현 후")
-    ResponseEntity<PlanDto.ReadResponse> readPlanById(@PathVariable Long planId);
+    ResponseEntity readPlanById(@PathVariable Long planId);
 
     @Operation(summary = "Plan 수정", description = " ")
-    ResponseEntity<PlanDto.UpdateResponse> updatePlan(@PathVariable Long planId, @RequestBody PlanDto.UpdateRequest request);
+    ResponseEntity updatePlan(@PathVariable Long planId, @RequestBody PlanDto.UpdateRequest request);
 
     @Operation(summary = "Plan 삭제", description = " ")
-    ResponseEntity<PlanDto.DeleteResponse> deletePlan(@PathVariable Long planId);
+    ResponseEntity deletePlan(@PathVariable Long planId);
 
 
 
@@ -37,16 +36,16 @@ public interface PlanControllerDocs {
 
 
     @Operation(summary = "DayPlan 작성", description = " ")
-    ResponseEntity<DayPlanDto.CreateResponse> createDayPlan(@PathVariable Long planId, @RequestBody DayPlanDto.CreateRequest request);
+    ResponseEntity createDayPlan(@PathVariable Long planId, @RequestBody DayPlanDto.CreateRequest request);
 
     @Operation(summary = "DayPlan 상세조회", description = " ")
-    ResponseEntity<DayPlanDto.ReadResponse> readDayPlan(@PathVariable Long planId, @PathVariable Long dayPlanId);
+    ResponseEntity readDayPlan(@PathVariable Long planId, @PathVariable Long dayPlanId);
 
     @Operation(summary = "DayPlan 수정", description = " ")
-    ResponseEntity<DayPlanDto.UpdateResponse> updateDayPlan(@PathVariable Long planId, @PathVariable Long dayPlanId, @RequestBody DayPlanDto.UpdateRequest request);
+    ResponseEntity updateDayPlan(@PathVariable Long planId, @PathVariable Long dayPlanId, @RequestBody DayPlanDto.UpdateRequest request);
 
     @Operation(summary = "DayPlan 삭제", description = " ")
-    ResponseEntity<DayPlanDto.DeleteResponse> deleteDayPlan(@PathVariable Long planId, @PathVariable Long dayPlanId);
+    ResponseEntity deleteDayPlan(@PathVariable Long planId, @PathVariable Long dayPlanId);
 
 
 
@@ -55,14 +54,14 @@ public interface PlanControllerDocs {
 
 
     @Operation(summary = "UnitPlan 작성", description = " ")
-    ResponseEntity<UnitPlanDto.CreateResponse> createUnitPlan(@PathVariable Long planId, @PathVariable Long dayPlanId, @RequestBody UnitPlanDto.CreateRequest request);
+    ResponseEntity createUnitPlan(@PathVariable Long planId, @PathVariable Long dayPlanId, @RequestBody UnitPlanDto.CreateRequest request);
 
     @Operation(summary = "UnitPlan 상세조회", description = " ")
-    ResponseEntity<UnitPlanDto.ReadResponse> readUnitPlan(@PathVariable Long planId, @PathVariable Long dayPlanId, @PathVariable Long unitPlanId);
+    ResponseEntity readUnitPlan(@PathVariable Long planId, @PathVariable Long dayPlanId, @PathVariable Long unitPlanId);
 
     @Operation(summary = "UnitPlan 수정", description = " ")
-    ResponseEntity<UnitPlanDto.UpdateResponse> updateUnitPlan(@PathVariable Long planId, @PathVariable Long dayPlanId, @PathVariable Long unitPlanId, @RequestBody UnitPlanDto.UpdateRequest request);
+    ResponseEntity updateUnitPlan(@PathVariable Long planId, @PathVariable Long dayPlanId, @PathVariable Long unitPlanId, @RequestBody UnitPlanDto.UpdateRequest request);
 
     @Operation(summary = "UnitPlan 삭제", description = " ")
-    ResponseEntity<UnitPlanDto.DeleteResponse> deleteUnitPlan(@PathVariable Long planId, @PathVariable Long dayPlanId, @PathVariable Long unitPlanId);
+    ResponseEntity deleteUnitPlan(@PathVariable Long planId, @PathVariable Long dayPlanId, @PathVariable Long unitPlanId);
 }
--- src/main/java/com/travelland/docs/TripControllerDocs.java
+++ src/main/java/com/travelland/docs/TripControllerDocs.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,9 +16,8 @@ public interface TripControllerDocs {
 
     @Operation(summary = "여행정보 등록", description = "작성한 여행정보를 등록하는 API")
     ResponseEntity createTrip(@RequestPart TripDto.CreateRequest requestDto,
-                              @RequestPart List<MultipartFile> imageList
-//                              @AuthenticationPrincipal UserDetailsImpl userDetails
-    );
+                              @RequestPart List<MultipartFile> imageList,
+                              @RequestPart String email);
 
     @Operation(summary = "여행정보 상세조회", description = "선택한 여행정보에 대한 내용을 조회하는 API")
     ResponseEntity getTrip(@PathVariable Long tripId);
@@ -33,12 +31,9 @@ public interface TripControllerDocs {
     @Operation(summary = "여행정보 수정", description = "작성한 여행정보에 대한 내용을 수정하는 API")
     ResponseEntity updateTrip(@PathVariable Long tripId,
                               @RequestPart TripDto.UpdateRequest requestDto,
-                              @RequestPart List<MultipartFile> imageList
-//                                     @AuthenticationPrincipal UserDetailsImpl userDetails
-    );
+                              @RequestPart List<MultipartFile> imageList,
+                              @RequestPart String email);
 
     @Operation(summary = "여행정보 삭제", description = "등록한 여행정보를 삭제하는 API")
-    ResponseEntity deleteTrip(@PathVariable Long tripId
-//                              @AuthenticationPrincipal UserDetailsImpl userDetails
-    );
+    ResponseEntity deleteTrip(@PathVariable Long tripId);
 }
