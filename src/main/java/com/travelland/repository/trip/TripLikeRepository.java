6c65912f9d30d9099a3b06a530bcb41b04cb5c59
cheesecup <kdy8252@naver.com>
2024-04-03 19:27:45 +0900

    REFACTOR: 상위 패키지 변경

--- src/main/java/com/travelland/repository/trip/TripLikeRepository.java
+++ src/main/java/com/travelland/repository/trip/TripLikeRepository.java
@@ -1,15 +1,19 @@
-package com.travelland.repository;
+package com.travelland.repository.trip;
 
 import com.travelland.domain.Member;
 import com.travelland.domain.Trip;
 import com.travelland.domain.TripLike;
+import com.travelland.repository.trip.querydsl.CustomTripLikeRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface TripLikeRepository extends JpaRepository<TripLike, Long> {
+public interface TripLikeRepository extends JpaRepository<TripLike, Long>, CustomTripLikeRepository {
 
     void deleteAllByTrip(Trip trip);
 
     Optional<TripLike> findByMemberAndTrip(Member member, Trip trip);
+
+    List<TripLike> getLikeListByMember(Member member, int size, int page);
 }
