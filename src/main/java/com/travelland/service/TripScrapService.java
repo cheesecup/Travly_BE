6295069fb5f08b9e5a953bda7e8e4e6ef7484201
cheesecup <kdy8252@naver.com>
2024-04-03 10:43:12 +0900

    FIX: 스크랩 등록/취소 상태 기능 개선
    기존 스크랩 취소시 스크랩 데이터 삭제 방식에서 `isDeleted` 컬럼을 추가하여 스크랩 상태 변경 방식으로 개선

--- src/main/java/com/travelland/service/TripScrapService.java
+++ src/main/java/com/travelland/service/TripScrapService.java
@@ -9,12 +9,13 @@ import com.travelland.repository.MemberRepository;
 import com.travelland.repository.TripRepository;
 import com.travelland.repository.TripScrapRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.travelland.domain.QTripScrap.*;
 
 @Service
 @RequiredArgsConstructor
@@ -25,32 +26,38 @@ public class TripScrapService {
     private final TripRepository tripRepository;
     private final JPAQueryFactory jpaQueryFactory;
 
-    //여행정보 스크랩 추가
+    //여행정보 스크랩 등록
     @Transactional
-    public void createTripScrap(Long tripId, String email) {
+    public void registerTripScrap(Long tripId, String email) {
         Member member = getMember(email);
         Trip trip = getTrip(tripId);
 
-        tripScrapRepository.save(new TripScrap(member, trip));
+        tripScrapRepository.findByMemberAndTrip(member, trip)
+                .ifPresentOrElse(
+                        TripScrap::registerScrap, // 스크랩을 한번이라도 등록한적이 있을경우
+                        () -> tripScrapRepository.save(new TripScrap(member, trip)) // 최초로 스크랩을 등록하는 경우
+                );
     }
 
-    //여행정보 스크랩 삭제
+    //여행정보 스크랩 취소
     @Transactional
-    public void deleteTripScrap(Long tripId, String email) {
+    public void cancelTripScrap(Long tripId, String email) {
         Member member = getMember(email);
         Trip trip = getTrip(tripId);
 
-        tripScrapRepository.deleteByMemberAndTrip(member, trip);
+        TripScrap tripScrap = tripScrapRepository.findByMemberAndTrip(member, trip)
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_SCRAP_NOT_FOUND));
+        tripScrap.cancelScrap();
     }
     
     //스크랩한 여행정보 목록 조회
     @Transactional(readOnly = true)
-    public List<TripDto.GetTripScrapListResponse> getTripScrapList(int page, int size, String sort, boolean ASC, String email) {
+    public List<TripDto.GetTripScrapListResponse> getTripScrapList(int page, int size, String email) {
         Member member = getMember(email);
 
-        List<TripScrap> tripScrapList = jpaQueryFactory.selectFrom(QTripScrap.tripScrap)
-                .where(QTripScrap.tripScrap.member.eq(member))
-                .orderBy(QTripScrap.tripScrap.trip.createdAt.desc())
+        List<TripScrap> tripScrapList = jpaQueryFactory.selectFrom(tripScrap)
+                .where(tripScrap.member.eq(member), tripScrap.isDeleted.eq(false))
+                .orderBy(tripScrap.trip.createdAt.desc())
                 .limit(size)
                 .offset((long) (page - 1) * size)
                 .fetch();
@@ -59,9 +66,9 @@ public class TripScrapService {
                 .collect(Collectors.toList());
     }
 
-    //여행정보 게시글 삭제시 해당 스크랩 데이터 삭제
+    //스크랩 데이터 삭제
     @Transactional
-    public void deleteTripScrapByTrip(Trip trip) {
+    public void deleteTripScrap(Trip trip) {
         tripScrapRepository.deleteAllByTrip(trip);
     }
 
