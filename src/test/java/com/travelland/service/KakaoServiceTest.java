283e128795c89ea45aed7b21a1ed22cf296136b1
cheesecup <kdy8252@naver.com>
2024-04-05 20:43:21 +0900

    FIX: Trip 성능 개선
    1. ES를 이용하여 검색 조회 성능 개선
    2. 코드 컨벤션 통일

--- src/test/java/com/travelland/service/KakaoServiceTest.java
+++ src/test/java/com/travelland/service/KakaoServiceTest.java
@@ -1,18 +1,11 @@
 //package com.travelland.service;
 //
-//import com.fasterxml.jackson.core.JsonProcessingException;
 //import com.travelland.dto.MemberDto;
-//import jakarta.servlet.http.HttpServletResponse;
 //import net.datafaker.Faker;
 //import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
 //
-//import java.util.UUID;
-//
-//import static org.junit.jupiter.api.Assertions.*;
 //@SpringBootTest
 //class KakaoServiceTest {
 //    @Autowired
