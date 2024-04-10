7104dddf54ef15ba35bf450b4257032e00b7bfea
itiswanthefull <itiswanthefull@gmail.com>
2024-04-10 22:14:28 +0900

    Fix: 코드윗미로 카카오로그인 이슈처리

--- src/main/java/com/travelland/repository/plan/PlanRepository.java
+++ src/main/java/com/travelland/repository/plan/PlanRepository.java
@@ -13,5 +13,9 @@ import java.util.Optional;
 public interface PlanRepository extends JpaRepository<Plan, Long>, CustomPlanRepository {
     Optional<Plan> findByIdAndIsDeletedAndIsPublic(Long planId, boolean isDeleted, boolean isPublic);
 
+    Optional<Plan> findByMemberIdAndIsDeleted(Long memberId, boolean isDeleted);
+
     Page<Plan> findAllByIsDeletedAndIsPublic(Pageable pageable, boolean isDeleted, boolean isPublic);
+
+    Page<Plan> findAllByMemberIdAndIsDeleted(Long memberId, Pageable pageable, boolean isDeleted);
 }
