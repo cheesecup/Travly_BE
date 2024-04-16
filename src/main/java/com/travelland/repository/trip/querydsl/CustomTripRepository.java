e45db2f1663b3953615f3e52bd785ffd356f570a
cheesecup <kdy8252@naver.com>
2024-04-16 21:50:39 +0900

    REFACTOR: 코드 리팩토링

--- src/main/java/com/travelland/repository/trip/querydsl/CustomTripRepository.java
+++ src/main/java/com/travelland/repository/trip/querydsl/CustomTripRepository.java
@@ -5,11 +5,11 @@ import com.travelland.domain.trip.Trip;
 
 import java.util.List;
 
-public interface CustomTripRepositoryV2 {
-
+public interface CustomTripRepository {
     List<Trip> getTripList(int page, int size, String sort, boolean ASC);
 
     List<Trip> getMyTripList(int page, int size, Member member);
 
     List<Trip> searchTripByHashtag(String hashtag, int page, int size, String sort, boolean ASC);
+
 }
