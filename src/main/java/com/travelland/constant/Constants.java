c50bcfac78f8a60ee5181991500ec76411435073
burning23185 <burning23@naver.com>
2024-04-20 20:38:18 +0900

    fix: 좋아요, 스크랩 캐쉬 전략 변경 read through, write through 사용

--- src/main/java/com/travelland/constant/Constants.java
+++ src/main/java/com/travelland/constant/Constants.java
@@ -15,7 +15,6 @@ public final class Constants {
     public static final String PLAN_LIKES_PLAN_ID = "planLikes:planId:";
     public static final String PLAN_LIKES_EMAIL = "planLikes:email:";
     public static final String PLAN_SCRAPS_PLAN_ID = "planScraps:planId:";
-    public static final String PLAN_SCRAPS_EMAIL = "planScraps:email:";
     public static final String PLAN_TOTAL_COUNT = "plan_total_count";
     public static final String TRIP_RECOMMEND_JOB_NAME = "tripRecommend";
     public static final String SEARCH_RANK_FIELD = "keyword";
