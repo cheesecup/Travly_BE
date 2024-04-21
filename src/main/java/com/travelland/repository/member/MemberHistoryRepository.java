560518e9c6fc6011f7f29cdad616ad2029c89640
burning23185 <burning23@naver.com>
2024-04-21 22:40:00 +0900

    fix: batch config 최적화 chunk size 조절 및 worker 수 조정

--- src/main/java/com/travelland/repository/member/MemberHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.travelland.repository.member;
-
-import com.travelland.domain.member.MemberHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface MemberHistoryRepository extends JpaRepository<MemberHistory, Long> {
-}
--- src/main/java/com/travelland/valid/trip/TripValidationGroups.java
+++
@@ -1,8 +0,0 @@
-package com.travelland.valid.trip;
-
-public class TripValidationGroups {
-    public interface TitleBlankGroup {}
-    public interface ContentBlankGroup {}
-    public interface CostRangeGroup {}
-    public interface AddressBlankGroup {}
-}
