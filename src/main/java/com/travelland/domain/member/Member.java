95806e4cbdc88cb06cf20b5b6015dd91e6f25eb2
Seungmi97 <mi.seung97@gmail.com>
2024-04-23 15:53:45 +0900

    FIX: 저장할 유저 정보 수정

--- src/main/java/com/travelland/domain/member/Member.java
+++ src/main/java/com/travelland/domain/member/Member.java
@@ -1,14 +1,11 @@
 package com.travelland.domain.member;
 
-import com.travelland.constant.Gender;
 import com.travelland.constant.Role;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -20,24 +17,11 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long socialId;
-
     @Column(length = 30)
     private String email;
 
-    private String password;
-
-    @Column(length = 8)
-    private String name;
-
     @Column(length = 15)
     private String nickname;
-
-    @Column(length = 8)
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
-
-    private LocalDate birth;
 
     @Column(length = 10)
     @Enumerated(EnumType.STRING)
@@ -46,14 +30,9 @@ public class Member {
     private String profileImage;
 
     @Builder
-    public Member(Long socialId, String email, String password, String name, String nickname, Gender gender, LocalDate birth, Role role, String profileImage) {
-        this.socialId = socialId;
+    public Member(String email, String nickname, Role role, String profileImage) {
         this.email = email;
-        this.password = password;
-        this.name = name;
         this.nickname = nickname;
-        this.gender = gender;
-        this.birth = birth;
         this.role = role;
         this.profileImage = profileImage;
     }
--- src/main/java/com/travelland/domain/member/MemberHistory.java
+++ src/main/java/com/travelland/domain/member/MemberHistory.java
@@ -1,6 +1,5 @@
 package com.travelland.domain.member;
 
-import com.travelland.constant.Gender;
 import com.travelland.constant.Role;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -17,37 +16,22 @@ public class MemberHistory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long socialId;
-
     @Column(length = 30)
     private String email;
 
-    private String password;
-
-    @Column(length = 8)
-    private String name;
-
     @Column(length = 15)
     private String nickname;
-
-    @Column(length = 8)
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
-
-    private LocalDate birth;
 
     @Column(length = 10)
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    private String profileImage;
+
     public MemberHistory(Member member) {
-        this.socialId = member.getSocialId();
         this.email = member.getEmail();
-        this.password = member.getPassword();
-        this.name = member.getName();
         this.nickname = member.getNickname();
-        this.gender = member.getGender();
-        this.birth = member.getBirth();
         this.role = member.getRole();
+        this.profileImage = member.getProfileImage();
     }
 }
